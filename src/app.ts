f46ea96854e0851ee0dcfbee46dcafa3b9c4d9ee
JoshuaPierre <nashucool01@gmail.com>
2025-01-13 18:51:15 -0500

    Etape 5 fini

--- src/app.ts
+++ src/app.ts
@@ -60,20 +60,24 @@ class App {
         });
     });
 
-    // Route pour classement (stats)
-    const joueurs: Array<Joueur> = JSON.parse(jeuRoutes.controleurJeu.joueurs);
-    // Calculer le ratio pour chaque joueur
-    const joueursAvecRatio = joueurs.map(joueur => {
-      const ratio = joueur.lancers > 0 ? joueur.lancersGagnes /joueur.lancers : 0; // Pour éviter la division par zéro
-      return { ...joueur, ratio }; // Ajouter le ratio à l'objet joueur
-    }).sort((a, b) => b.ratio - a.ratio);
+    
 
-    //console.log("joueurs "+ jeuRoutes.controleurJeu.joueurs)
-
-    //console.log("ratio "+joueursAvecRatio)
+   
 
     
     router.get('/stats', (req, res, next) => {
+
+      // Route pour classement (stats)
+      const joueurs: Array<Joueur> = JSON.parse(jeuRoutes.controleurJeu.joueurs);
+      // Calculer le ratio pour chaque joueur
+      const joueursAvecRatio = joueurs.map(joueur => {
+        const ratio = joueur.lancers > 0 ? joueur.lancersGagnes /joueur.lancers : 0; // Pour éviter la division par zéro
+        return { ...joueur, ratio }; // Ajouter le ratio à l'objet joueur
+      }).sort((a, b) => b.ratio - a.ratio);
+
+      console.log("joueurs "+ jeuRoutes.controleurJeu.joueurs)
+      console.log("ratio "+joueursAvecRatio)
+
       res.render('stats',
         // passer objet au gabarit (template) Pug
         {
--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -3,6 +3,7 @@ import { assert } from 'console';
 import 'jest-extended';
 import supertest from 'supertest';
 import app from '../../src/app';
+import { JeuDeDes } from '../../src/core/jeuDeDes';
 import { Joueur } from '../../src/core/joueur';
 
 const request = supertest(app);
@@ -23,10 +24,19 @@ describe('GET /api/v1/jeu/redemarrerJeu', () => {
     joueur2 = new Joueur('Mike')
   });
 
-  it(`devrait etre le premier test.`, async () => {
+  it(`Devrait etre le premier test.`, async () => {
     const response = await request.get('/api/v1/jeu/redemarrerJeu');
     expect(response.statusCode).toBe(200);
     expect(response.headers['content-type']).toMatch(/application\/json/);
-    expect(response.body).toEqual({});
+    expect(response.body).toEqual({ "message": "Success"});
+  });
+
+  it(`Devrait verifier qui ny aie plus de joueur apres le redemarrage.`, async () => {
+   let jeuDeDes = new JeuDeDes();
+   jeuDeDes.demarrerJeu(joueur1.nom)
+   jeuDeDes.demarrerJeu(joueur2.nom)
+
+   jeuDeDes.redemarrerJeu()
+   expect(jeuDeDes['_joueurs'].size).toEqual(0);
   });
 });
